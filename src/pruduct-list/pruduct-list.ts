b6064e78ea135a953c0614a16042fbc3e16ce6fb
SimonGirado <145472376+SimonGirado@users.noreply.github.com>
2025-07-15 14:14:34 -0300

    Add files via upload

---
+++ src/pruduct-list/pruduct-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pruduct-list',
+  imports: [],
+  templateUrl: './pruduct-list.html',
+  styleUrl: './pruduct-list.scss'
+})
+export class PruductList {
+
+}
